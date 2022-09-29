0276b00b8c1cdd452b064ab965d1a5af63c889d2
Isaac Morales <isaacmoralessantana@gmail.com>
2022-09-29 10:14:20 +0200

    Terminado ejercicio 12

---
+++ Sesion 2/12_Tarifa_aérea_descuento.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    double precio;
+    cin >> precio;
+
+    double descuento1 = precio*0.96;
+    double descuento2 = precio*0.98;
+
+    cout << descuento1 << " "  << descuento2 << endl;
+
+    return 0;
+}
