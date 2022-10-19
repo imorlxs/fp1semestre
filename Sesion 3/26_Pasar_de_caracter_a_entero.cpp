dc924f92083883bb2b7f05fa1cd30e75a1c545f5
imorlxs <isaacmoralessantana@gmail.com>
2022-10-19 10:19:18 +0200

    Realizado Ejercicio 26

---
+++ Sesion 3/26_Pasar_de_caracter_a_entero.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int entero;
+    char caracter;
+
+    cin >> caracter;
+    entero = caracter;
+
+    cout << entero - '0' << endl;
+
+    return 0;
+}
