733c3493cb323629b84a22057e241992e1d74e45
Isaac Morales <isaacmoralessantana@gmail.com>
2022-10-19 14:16:57 +0200

    Terminado Ejercicio 2

--- Relación II/Sesión 3/2_Se_dividen.cpp
+++ Relación II/Sesión 3/2_Se_dividen.cpp
@@ -2,6 +2,14 @@
 using namespace std;
 
 int main(){
+    int numero1, numero2;
+    cin >> numero1 >> numero2;
 
+    if (numero1 % numero2 == 0 || numero2 % numero1 == 0){
+        cout << "Uno de ellos divide al otro" << endl;
+    }
+    else {
+        cout << "Ninguno divide al otro" << endl;
+    }
     return 0;
 }
